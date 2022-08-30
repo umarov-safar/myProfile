a95639ce18d592f41c3146b187cd568ae6fc4e52
Safar  Umarov <68920816+umarov-safar@users.noreply.github.com>
2022-08-30 16:22:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Teacher", "PHP developer"],
+        strings:  ["Fullstack developer", "Developer", "PHP developer", "Laravel developer", "React, tailwincss"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "PHP developer", "Teacher"],
+        strings: ["Fullstack developer", "Developer", "PHP developer", "Laravel developer", "React, tailwincss"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
